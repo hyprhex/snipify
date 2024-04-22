76856eac43afb074bd022a1e12372585330bf002
hyprhex <swapapis@gmail.com>
2024-04-22 07:14:17 +0100

    Add the base template and rewrite the home to invoke into the base

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,13 +14,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	files := []string{
+		"./ui/html/pages/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
