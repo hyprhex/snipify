ad7b24d5d2c69e8b25c5b611fd8da0d8e2ca337b
hyprhex <swapapis@gmail.com>
2024-05-18 10:30:43 +0100

    Design database model and create a connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,11 +9,13 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/hyprhex/snipify/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -35,6 +37,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
